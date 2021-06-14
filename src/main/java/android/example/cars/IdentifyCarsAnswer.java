e2aa7037ebce787edf14f02fbf09cf0ac2bdfa24
Supipi Nelumika <66358229+Supipi622@users.noreply.github.com>
2021-06-14 07:48:12 +0530

    Add files via upload

---
+++ src/main/java/android/example/cars/IdentifyCarsAnswer.java
@@ -0,0 +1,14 @@
+package android.example.cars;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IdentifyCarsAnswer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identify_cars_answer);
+    }
+}
